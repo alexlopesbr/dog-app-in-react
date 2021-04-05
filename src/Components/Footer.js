4bbfed2db3d75bdea098951e02e9e5bd7d2e298e
Lopes <criabio@hotmail.com>
2021-04-05 17:01:11 -0300

    header style

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = (props) => {
+  return <div className={styles.footer}>Footer</div>;
+};
+
+Footer.propTypes = {};
+
+export default Footer;
