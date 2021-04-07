fa5a3697bdf7c879b611a8db0127f9c9e320a67e
alexlopesbr <criabio@hotmail.com>
2021-04-07 08:52:02 -0300

    cadastrar usuario

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,11 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha deve conter no mínimo 8 caracteres, uma letra maiúscula, uma letra minúscula e um caracter expecial',
+  },
 };
 
 const useForm = (type) => {
